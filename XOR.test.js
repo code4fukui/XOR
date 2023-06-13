2b38b4a36fa6ebfe98de06ba0d5ee00014d7e434
Taisuke Fukuno <fukuno@jig.jp>
2023-06-13 10:11:03 +0900

    add test

--- XOR.test.js
+++ XOR.test.js
@@ -9,3 +9,12 @@ Deno.test("simple", () => {
 	const dec = XOR.convert(data, pass);
 	t.assertEquals(data, dec);
 });
+Deno.test("exception if null", () => {
+	const data = new TextEncoder().encode("ABCDEFGあいうえお");
+	const pass = new Uint8Array([1, 3, 5]);
+	t.assertThrows(() => XOR.convert(data, null));
+	t.assertThrows(() => XOR.convert(null, null));
+	t.assertThrows(() => XOR.convert(null, pass));
+	t.assertThrows(() => XOR.convert());
+	t.assertThrows(() => XOR.convert(data));
+});
