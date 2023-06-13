d2b86ffc9f8edfca0520f50806b9da354957186e
Taisuke Fukuno <fukuno@jig.jp>
2023-06-13 10:17:49 +0900

    tab to space

--- XOR.test.js
+++ XOR.test.js
@@ -2,19 +2,19 @@ import * as t from "https://deno.land/std/testing/asserts.ts";
 import { XOR } from "./XOR.js";
 
 Deno.test("simple", () => {
-	const data = new TextEncoder().encode("ABCDEFGあいうえお");
-	const pass = new Uint8Array([1, 3, 5]);
-	const enc = XOR.convert(data, pass);
-	//console.log(new TextDecoder().decode(enc));
-	const dec = XOR.convert(data, pass);
-	t.assertEquals(data, dec);
+  const data = new TextEncoder().encode("ABCDEFGあいうえお");
+  const pass = new Uint8Array([1, 3, 5]);
+  const enc = XOR.convert(data, pass);
+  //console.log(new TextDecoder().decode(enc));
+  const dec = XOR.convert(data, pass);
+  t.assertEquals(data, dec);
 });
 Deno.test("exception if null", () => {
-	const data = new TextEncoder().encode("ABCDEFGあいうえお");
-	const pass = new Uint8Array([1, 3, 5]);
-	t.assertThrows(() => XOR.convert(data, null));
-	t.assertThrows(() => XOR.convert(null, null));
-	t.assertThrows(() => XOR.convert(null, pass));
-	t.assertThrows(() => XOR.convert());
-	t.assertThrows(() => XOR.convert(data));
+  const data = new TextEncoder().encode("ABCDEFGあいうえお");
+  const pass = new Uint8Array([1, 3, 5]);
+  t.assertThrows(() => XOR.convert(data, null));
+  t.assertThrows(() => XOR.convert(null, null));
+  t.assertThrows(() => XOR.convert(null, pass));
+  t.assertThrows(() => XOR.convert());
+  t.assertThrows(() => XOR.convert(data));
 });
