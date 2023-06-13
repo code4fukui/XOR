4101cc9eab2f2836e7cee4d2e5546f41cdd276e2
Taisuke Fukuno <fukuno@jig.jp>
2023-06-13 10:17:37 +0900

    tab to space

--- XOR.js
+++ XOR.js
@@ -1,7 +1,7 @@
 const convertXOR = (data, pass) => {
-	for (let i = 0; i < data.length; i++) {
-		data[i] ^= pass[i % pass.length];
-	}
+  for (let i = 0; i < data.length; i++) {
+    data[i] ^= pass[i % pass.length];
+  }
   return data;
 };
 
